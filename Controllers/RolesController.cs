cda1256d7b8ff7debd625b23654621f2d8d795bb
flows0n <flovvek@gmail.com>
2021-06-13 18:02:39 +0200

    Próba naprawy, dodanie roli

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using ProjectApp.Models;
+using System.Web.Mvc;
 
 namespace ProjectApp.Controllers
 {
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -26,88 +26,130 @@ namespace ProjectApp.Models
         public virtual ICollection <Favorite> Favorites { get; set; }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-        public DbSet <Category> Categories { get; set; }
-
-        public DbSet <Product> Products { get; set; }
-
-        public DbSet<Keyword> Keywords { get; set; }
-
-        public DbSet<KeywordCategory> KeywordCategories { get; set; }
-
-        public DbSet <UserProduct> UserProducts { get; set; }
-
-        public DbSet <Favorite> Favorites { get; set; }
-
-		public bool RoleExists(string name)
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
 		{
-			var rm = LocalRoleManager;
-
-			return rm.RoleExists(name);
 		}
 
-
-		public bool CreateRole(string name)
+		public static ApplicationDbContext Create()
 		{
-			var rm = LocalRoleManager;
-			var idResult = rm.Create(new IdentityRole(name));
-
-			return idResult.Succeeded;
+			return new ApplicationDbContext();
 		}
+		public DbSet<Category> Categories { get; set; }
 
+		public DbSet<Product> Products { get; set; }
 
-		public bool CreateUser(ApplicationUser user, string password)
+		public DbSet<Keyword> Keywords { get; set; }
+
+		public DbSet<KeywordCategory> KeywordCategories { get; set; }
+
+		public DbSet<UserProduct> UserProducts { get; set; }
+
+		public DbSet<Favorite> Favorites { get; set; }
+	}
+		//Klasa dodana
+		public class IdentityManager
 		{
-			var um = LocalUserManager;
-			var idResult = um.Create(user, password);
-
-			return idResult.Succeeded;
-		}
-
-
-		public bool AddUserToRole(string userId, string roleName)
-		{
-			var um = LocalUserManager;
-			var idResult = um.AddToRole(userId, roleName);
-
-			return idResult.Succeeded;
-		}
-
-
-		public bool AddUserToRoleByUsername(string username, string roleName)
-		{
-			var um = LocalUserManager;
-
-			string userID = um.FindByName(username).Id;
-			var idResult = um.AddToRole(userID, roleName);
-
-			return idResult.Succeeded;
-		}
-
-
-		public void ClearUserRoles(string userId)
-		{
-			var um = LocalUserManager;
-			var user = um.FindById(userId);
-			var currentRoles = new List<IdentityUserRole>();
-
-			currentRoles.AddRange(user.Roles);
-
-			foreach (var role in currentRoles)
+			public RoleManager<IdentityRole> LocalRoleManager
 			{
-				um.RemoveFromRole(userId, role.RoleId);
+				get
+				{
+					return new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+				}
+			}
+
+
+			public UserManager<ApplicationUser> LocalUserManager
+			{
+				get
+				{
+					return new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+				}
+			}
+
+
+			public ApplicationUser GetUserByID(string userID)
+			{
+				ApplicationUser user = null;
+				UserManager<ApplicationUser> um = this.LocalUserManager;
+
+				user = um.FindById(userID);
+
+				return user;
+			}
+
+
+			public ApplicationUser GetUserByName(string email)
+			{
+				ApplicationUser user = null;
+				UserManager<ApplicationUser> um = this.LocalUserManager;
+
+				user = um.FindByEmail(email);
+
+				return user;
+			}
+
+
+			public bool RoleExists(string name)
+			{
+				var rm = LocalRoleManager;
+
+				return rm.RoleExists(name);
+			}
+
+
+			public bool CreateRole(string name)
+			{
+				var rm = LocalRoleManager;
+				var idResult = rm.Create(new IdentityRole(name));
+
+				return idResult.Succeeded;
+			}
+
+
+			public bool CreateUser(ApplicationUser user, string password)
+			{
+				var um = LocalUserManager;
+				var idResult = um.Create(user, password);
+
+				return idResult.Succeeded;
+			}
+
+
+			public bool AddUserToRole(string userId, string roleName)
+			{
+				var um = LocalUserManager;
+				var idResult = um.AddToRole(userId, roleName);
+
+				return idResult.Succeeded;
+			}
+
+
+			public bool AddUserToRoleByUsername(string username, string roleName)
+			{
+				var um = LocalUserManager;
+
+				string userID = um.FindByName(username).Id;
+				var idResult = um.AddToRole(userID, roleName);
+
+				return idResult.Succeeded;
+			}
+
+
+			public void ClearUserRoles(string userId)
+			{
+				var um = LocalUserManager;
+				var user = um.FindById(userId);
+				var currentRoles = new List<IdentityUserRole>();
+
+				currentRoles.AddRange(user.Roles);
+
+				foreach (var role in currentRoles)
+				{
+					um.RemoveFromRole(userId, role.RoleId);
+				}
 			}
 		}
 	}
-
-}
